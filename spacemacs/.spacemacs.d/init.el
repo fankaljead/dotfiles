f14d64540450fa6c14f6e93cc6718fd669fadfbe
周翔辉 <3236432-fankaljead@users.noreply.gitlab.com>
2020-04-25 09:59:58 +0800

    add rofi clipboard

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -505,6 +505,7 @@ you should place your code here."
 
   (setq-default web-mode-markup-indent-offset 2
                 web-mode-css-indent-offset 2
+                js2-basic-offset 2
                 web-mode-code-indent-offset 2
                 web-mode-attr-indent-offset 2)
 
