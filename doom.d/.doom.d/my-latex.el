8e977b0289eb9fe32e0875458d692342bf93448f
fankaljead <728678732@qq.com>
2020-07-07 13:33:57 +0800

    zsh config

--- doom.d/.doom.d/my-latex.el
+++ doom.d/.doom.d/my-latex.el
@@ -1,1 +1,8 @@
 ;;; ~/.doom.d/my-latex.el -*- lexical-binding: t; -*-
+;;
+;; #+LATEX_COMPILER: xelatex
+;; #+LATEX_HEADER: \usepackage{ctex}
+;; (setq org-latex-to-pdf-process
+(setq org-latex-to-pdf-process
+  '("xelatex -interaction nonstopmode %f"
+     "xelatex -interaction nonstopmode %f")) ;
